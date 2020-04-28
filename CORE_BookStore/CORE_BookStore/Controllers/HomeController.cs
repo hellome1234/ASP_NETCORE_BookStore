97c881329ab3f243ed61e8710d2229f32aa4da7b
Hrishav Tiwari <testingtesting3423@gmail.com>
2020-04-28 11:37:52 +0545

    Added MVC to application

---
+++ CORE_BookStore/CORE_BookStore/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CORE_BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public String Index()
+        {
+            return "BookStore";
+        }
+    }
+}
